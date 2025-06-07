4c9d5ca92da198f56ce0079be8e37c033c2ee7ab
Mateo del Rio <mdelriolanse@gmail.com>
2025-06-07 18:44:33 -0400

    Committed accelerator detection script. Determine available pytorch-compatible accelerator hardward (e.g., CUDA (NVIDIA), XPU (INTEL), MPS (MAC)).

---
+++ dartopt/utils/accelerator.py
@@ -0,0 +1,16 @@
+import torch
+
+def get_accelerator():
+
+    assert torch.accelerator.is_available(), "No available accelerators detected."
+
+    if torch.cuda.is_available():
+        torch.set_default_device("cuda")
+    elif torch.mtia.is_available():
+        torch.set_default_device("mtia")
+    elif torch.xpu.is_available():
+        torch.set_default_device("xpu")
+    elif torch.mps.is_available():
+        torch.set_default_device("mps")
+
+    return torch.get_default_device()
